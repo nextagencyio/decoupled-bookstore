bb1c6540c2edab480b53d63f7d6e169dd1efd261
Jay Callicott <jrcallicott@gmail.com>
2026-03-08 08:42:45 -0500

    fix: mark header-dependent routes as dynamic

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -7,6 +7,7 @@ import Header from '../components/Header'
 import BookCard from '../components/BookCard'
 
 export const revalidate = 3600
+export const dynamic = 'force-dynamic'
 
 export const metadata: Metadata = {
   title: 'Books | The Gilded Page',
